b3ebf8584fb398d398dadf1f6cd825fe1dd67d17
veto <wardhana098@gmail.com>
2024-07-26 11:22:30 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var gambarUtama=document.getElementById("utama");
+
+function gambar1(){
+    gambarUtama.setAttribute("src", "asset/c16.jpg");
+}
+function gambar2(){
+    gambarUtama.setAttribute("src", "asset/c17.jpg");
+}
+function gambar3(){
+    gambarUtama.setAttribute("src", "asset/c18.jpg");
+}
+function gambar4(){
+    gambarUtama.setAttribute("src", "asset/c19.jpg");
+}
+function gambar5(){
+    gambarUtama.setAttribute("src", "asset/c20.jpg");
+}
